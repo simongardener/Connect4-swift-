e672c285c5d2e46454c17992cb43627d206d9265
Simon Gardener <simongardener@me.com>
2017-07-04 16:56:57 +0100

    Initial Commit

---
+++ Connect4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Simon Gardener on 04/07/2017.
+//  Copyright © 2017 Simon Gardener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
